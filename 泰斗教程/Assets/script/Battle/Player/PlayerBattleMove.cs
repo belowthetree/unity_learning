a4e7185bc6c7312aefe2275f8b0271a134c678e7
belowthetree <37807774+belowthetree@users.noreply.github.com>
2019-02-21 16:32:02 +0800

    增加并修复了特效

---
+++ 泰斗教程/Assets/script/Battle/Player/PlayerBattleMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBattleMove : MonoBehaviour {
+
+    public float speed = 5f;
+    public Animator ani;
+    
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 vec = this.GetComponent<Rigidbody>().velocity;
+
+        if (Mathf.Abs(h) > 0.05f || Mathf.Abs(v) > 0.05f)
+        {
+            ani.SetBool("move", true);
+            this.GetComponent<Rigidbody>().velocity = new Vector3(speed * h, vec.y, speed * v);
+            transform.LookAt(new Vector3(h, 0, v) + transform.position);
+        }
+        else
+        {
+            ani.SetBool("move", false);
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0, vec.y, 0);
+        }
+    }
+}
---
+++ 泰斗教程/Assets/script/Battle/SkillButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButton : MonoBehaviour {
+
+    public PosType pos = PosType.Basic;
+	public PlayerAnimation playerAnimation;
+    public float coldTime = 4f;
+    public float coldTimer = 0;
+    public Image mask;
+    public bool isPress = false;
+    
+    void Update()
+    {
+        if (mask == null)
+            return;
+        if(coldTimer>0)
+        {
+            coldTimer -= Time.deltaTime;
+            mask.fillAmount = coldTimer/coldTime;
+        }
+        else
+        {
+            isPress = false;
+            mask.fillAmount = 0;
+            this.GetComponent<Button>().interactable = true;
+        }
+    }
+
+    public void OnPress()
+    {
+        if (isPress == false&&mask!=null)
+        {
+            isPress = true;
+            playerAnimation.OnAttackButtonClick(isPress, pos);
+            coldTimer = coldTime;
+            this.GetComponent<Button>().interactable = false;
+        }
+    }
+}
